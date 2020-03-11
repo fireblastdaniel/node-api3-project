228ccf8dc0ed655ad93e590f9ce2f2a7a9fbcbe5
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-03-11 16:59:20 -0700

    get requests on userRouter are working

--- users/userRouter.js
+++ users/userRouter.js
@@ -11,16 +11,40 @@ router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+//200
+//500
+//WORKING
 router.get('/', (req, res) => {
-  // do your magic!
+  Users.get()
+    .then(resources => {
+      res.status(200).json({ resources });
+    })
+    .catch(error =>
+      res
+        .status(500)
+        .json({ message: 'There was an error retrieving the user data' })
+    );
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+//200
+//400 --> validateUserId
+//500 --> validateUserId
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+//200
+//400 --> validateUserId
+//500 --> validateUserId,
+router.get('/:id/posts', validateUserId, (req, res) => {
+  const { id } = req.params;
+  Users.getUserPosts(id)
+    .then(resources => res.status(200).json(resources))
+    .catch(error =>
+      res
+        .status(500)
+        .json({ message: 'There was an error retrieving the posts.' })
+    );
 });
 
 router.delete('/:id', (req, res) => {
@@ -34,20 +58,25 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
-  const userId = req.body.id;
-  Users.getById(userId).then(resource => {
-    if (resource) {
-      req.user = resource;
-      next();
-    } else {
-      res.status(400).json({ message: 'invalid user id' });
-    }
-  });
+  const { id } = req.params;
+  Users.getById(id)
+    .then(resource => {
+      if (resource) {
+        req.user = resource;
+        next();
+      } else {
+        res.status(400).json({ message: 'invalid user id' });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: 'There was an error retrieving the user' });
+    });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
   const user = req.body;
   !user ? res.status(400).json({ message: 'missing user data' }) : null;
   !user.name
@@ -57,7 +86,6 @@ function validateUser(req, res, next) {
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
   const post = req.body;
   !post ? res.status(400).json({ message: 'missing post data' }) : null;
   !post.text
